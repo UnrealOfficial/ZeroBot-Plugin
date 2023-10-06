e6c9a1e5cd48bbac1f47f0fcd7c354b484f4a984
星野幻境 <105903609+UnrealOfficial@users.noreply.github.com>
2023-10-06 20:19:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -258,7 +258,7 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
+	// sus = append(sus, 2582766743)
 	// sus = append(sus, 87654321)
 
 	// 启用 webui
@@ -287,7 +287,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "DELUXE KUMA", "Deluxe Kuma", "迪拉熊", "でらっくま"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
